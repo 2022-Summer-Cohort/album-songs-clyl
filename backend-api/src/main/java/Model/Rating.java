0f1185935529c8b25357bc1b7e3ba247bf62a46b
chandanbihagpatel <chandanbihagpatel@gmail.com>
2022-07-18 15:24:18 -0400

    almost done with models

--- backend-api/src/main/java/Model/Rating.java
+++ backend-api/src/main/java/Model/Rating.java
@@ -1,4 +1,58 @@
 package Model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
 public class Rating {
+    @Id
+    @GeneratedValue
+
+    private Long id;
+    private String review;
+    private int rating;
+    @ManyToOne
+    private Song song;
+
+    public Rating(String review, int rating, Song song) {
+        this.review = review;
+        this.rating = rating;
+        this.song = song;
+    }
+
+    public Rating(){}
+
+    public String getReview() {
+        return review;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public Song getSong() {
+        return song;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rating rating1 = (Rating) o;
+        return rating == rating1.rating && Objects.equals(review, rating1.review) && Objects.equals(song, rating1.song);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(review, rating, song);
+    }
 }
--- backend-api/src/main/java/Model/Song.java
+++ backend-api/src/main/java/Model/Song.java
@@ -1,4 +1,54 @@
 package Model;
 
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
 public class Song {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private int duration;
+    @ManyToOne
+    private Album album;
+    @ManyToOne
+    private Artist artist;
+    @OneToMany (mappedBy = "song")
+    private Collection<Rating> ratings;
+
+    public Song(String name, int duration, Album album, Artist artist) {
+        this.name = name;
+        this.duration = duration;
+        this.album = album;
+        this.artist = artist;
+    }
+
+    public Song(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public Collection<Rating> getRatings() {
+        return ratings;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
+
